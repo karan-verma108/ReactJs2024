593f68d698e5ea1c32656124050b12d031442826
karan <karanverma201411@gmail.com>
2024-02-26 06:51:46 +0530

    usenavigate hook

--- src/ReactRouter/User.jsx
+++ src/ReactRouter/User.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 const User = () => {
     const { fname, lname } = useParams();
     const location = useLocation();
-    // console.log(location);
+    const navigate = useNavigate();
+    console.log(` navigate ${navigate}`);
+    console.log(location);
 
     return (
         <>
             <h1>Welcome {fname} {lname} to the app! </h1>
             <p>My current path is : {location.pathname}</p>
-            {location.pathname === `/users/karan/verma` ? <button onClick={()=> alert('yes you reached the right path')}>Click me</button> : null}
+            {location.pathname === `/users/karan/verma` ? <button onClick={() => alert('you reached at the right path')}>click me</button> : null}
+            {location.pathname === `/users/karan/verma` ? <button onClick={() => navigate('/')}>Homepage</button> : null}
         </>
     )
 }
