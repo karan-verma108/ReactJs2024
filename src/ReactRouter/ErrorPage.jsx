cfb31e1f21a8cf9afcaa6acba047eefeb182d66e
karan <karanverma201411@gmail.com>
2024-02-28 09:15:15 +0530

    search image changes

--- src/ReactRouter/ErrorPage.jsx
+++ src/ReactRouter/ErrorPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const ErrorPage = () =>{
-    return(
+const ErrorPage = () => {
+
+    const navigate = useNavigate();
+
+    return (
         <>
-        <h1 style={{'textAlign' : 'center'}}>OOPS PAGE NOT FOUND!!!</h1>
+            <h1 style={{ 'textAlign': 'center' }}>OOPS PAGE NOT FOUND!!!</h1>
+            <button onClick={() => navigate('/')}>Go back</button>
         </>
     )
 }
