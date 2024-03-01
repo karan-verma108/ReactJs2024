9ec60ceb0b6855fb0701955c69539d4be1d25417
karan <karanverma201411@gmail.com>
2024-03-01 06:04:26 +0530

    redirect added

--- src/ReactRouter/App.jsx
+++ src/ReactRouter/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
 import Name from "./Name";
-import ErrorPage from "./ErrorPage";
+// import ErrorPage from "./ErrorPage";
 import Navbar from "./Navbar";
 import Services from "./Services";
 import User from "./User";
@@ -22,7 +22,8 @@ const App = () => {
                 <Route path="/services" element={<Services title='Service' />} />
                 <Route path="/users/:fname/:lname" element={<User />} />
                 <Route path="/search" element={<Search />} />
-                <Route path="*" element={<ErrorPage />} />
+                {/* <Route path="*" element={<ErrorPage />} /> */}   {/* using this we can instead just show an error page */}
+                <Route path="*" element={<Navigate to='/' />} />  {/*using this we can redirect users to homepage if user enters a page that is not created*/}
             </Routes>
         </>
     )
